462b9abd1bf7dc32f2ab17edb29e774f54135951
lhw <lhw@hzzenith.com.cn>
2024-12-23 21:17:03 +0800

    初始化

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+      ? 'https://warlordlee.github.io/happy-new-year/'
+      : '/',
   outputDir: 'docs', // 输出文件目录
   plugins: [
     vue(),
