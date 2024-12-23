369d448330350dd108038d85e25d24d7bf867af7
lhw <lhw@hzzenith.com.cn>
2024-12-23 20:51:48 +0800

    初始化

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicPath: './',
+  outputDir: 'docs', // 输出文件目录
   plugins: [
     vue(),
     vueDevTools(),
